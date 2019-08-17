c2f3f6ff49034823af63255b9e172e4759166b5d
Krysik <marcin.krysik11@gmail.com>
2019-08-17 21:43:01 +0200

    add text editor in create note template, update css, add some widgets in AddNoteForm

--- notes/notekeeper/forms.py
+++ notes/notekeeper/forms.py
@@ -6,7 +6,13 @@ class AddNoteForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AddNoteForm, self).__init__(*args, **kwargs)
         self.fields['title'].label = 'Tytuł'
+        self.fields['title'].widget = forms.TextInput(
+            attrs={'placeholder': 'max 50 znaków'}
+        )
         self.fields['content'].label = 'Treść'
+        self.fields['content'].widget = forms.Textarea(attrs={
+            'id': 'editor'
+        })
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
 
