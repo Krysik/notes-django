fed59a8d9119930a0ed816c764e72bc036a87bed
Krysik <marcin.krysik11@gmail.com>
2019-08-31 10:19:56 +0200

    double quotes to single quotes

--- notes/settings.py
+++ notes/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = '/static/'
 LOGIN_URL = '/accounts/login'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
+
