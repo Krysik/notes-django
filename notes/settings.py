60a5828661c682e9dbbfb0eba8414b85f9296a62
Krysik <marcin.krysik11@gmail.com>
2019-09-03 17:35:36 +0200

    switch DEBUG mode

--- notes/settings.py
+++ notes/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dj-notes.herokuapp.com']
 SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('FB_APP_ID')
