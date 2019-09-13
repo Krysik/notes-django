201242b501f00a4ce60f2e588bbe357db64cab4c
Krysik <marcin.krysik11@gmail.com>
2019-09-14 00:36:13 +0200

    Django sends an email after user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.urls import reverse
 from .forms import RegisterForm, LoginForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 def index_view(request):
     return render(request, 'index.html', {})
 
@@ -13,10 +20,37 @@ def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Twoje konto zostało utworzone')
-            return redirect('login_view')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Dj-Notes - Aktywuj swoje konto'
+            message = render_to_string('accounts/confirm_an_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return render(request, 'accounts/confirm_an_email_msg.html')
     return render(request, 'accounts/register.html', {'form': form})
+
+def activate_an_email(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+    return render(request, 'accounts/confirm_an_email_succeed.html')
+
 
 def login_view(request):
     form = LoginForm()
